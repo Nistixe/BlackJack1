3736cdff7e140a3af9dd11c5f715465e6f65d9d2
Yılmaz Uğur Gülmez <yilmazugurg@gmail.com>
2025-10-11 11:58:29 +0300

    V2

---
+++ 06121c7fcd4d960a463084c89e6f503c1bb3f97b
@@ -0,0 +1,41 @@
+[33mcommit 33c76dc392de3391fe24c1fc46f62019a57e5c60[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Yılmaz Uğur Gülmez <152120231083@ogrenci.ogu.edu.tr>
+Date:   Tue Oct 7 14:32:49 2025 +0300
+
+    update4
+
+[33mcommit 06121c7fcd4d960a463084c89e6f503c1bb3f97b[m
+Author: Yılmaz Uğur Gülmez <152120231083@ogrenci.ogu.edu.tr>
+Date:   Tue Oct 7 14:28:55 2025 +0300
+
+    update3.1
+
+[33mcommit 53383947a2f990eef9ec63ddda92188c82f0cff1[m
+Author: Yılmaz Uğur Gülmez <152120231083@ogrenci.ogu.edu.tr>
+Date:   Tue Oct 7 14:27:44 2025 +0300
+
+    update3
+
+[33mcommit 5b060cf7062a2f2f56f051adb0b9149305a28db2[m
+Author: Yılmaz Uğur Gülmez <152120231083@ogrenci.ogu.edu.tr>
+Date:   Tue Oct 7 14:26:37 2025 +0300
+
+    update2
+
+[33mcommit 9e527fcd6f8207bb3cd14b8a9872f765e84c9ff8[m
+Author: Yılmaz Uğur Gülmez <152120231083@ogrenci.ogu.edu.tr>
+Date:   Sun Oct 5 16:21:39 2025 +0300
+
+    update1
+
+[33mcommit 942458b24bcedf39e4ab171d14ab664e4c968280[m
+Author: yilma <yilma@YILMAZ>
+Date:   Sun Oct 5 13:38:02 2025 +0300
+
+    Proje dosyası ekle.
+
+[33mcommit 474b58bb42790982548c55e9bc6a817203b2e39d[m
+Author: yilma <yilma@YILMAZ>
+Date:   Sun Oct 5 13:38:00 2025 +0300
+
+    .gitattributes ve .gitignore ekleyin.
---
+++ BlackJack1/BlackJack_V2.py
@@ -0,0 +1,112 @@
+﻿#V2
+import random
+import os
+# yorum 
+#yorum2
+class BlackjackGame:
+    def __init__(self):
+        self.deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
+        random.shuffle(self.deck)
+        self.dealer_hand = []
+        self.player_hand = []
+
+    def deal_card(self):
+        card = self.deck.pop()
+        return {11: "J", 12: "Q", 13: "K", 14: "A"}.get(card, card)
+
+    def deal_hand(self):
+        return [self.deal_card(), self.deal_card()]
+
+    def calculate_total(self, hand):
+        total = 0
+        ace_count = 0
+        for card in hand:
+            if card in ["J", "Q", "K"]:
+                total += 10
+            elif card == "A":
+                ace_count += 1
+                total += 11
+            else:
+                total += card
+        while total > 21 and ace_count:
+            total -= 10
+            ace_count -= 1
+        return total
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def print_results(self):
+        self.clear_screen()
+        print(f"Dealer's hand: {self.dealer_hand} (Total: {self.calculate_total(self.dealer_hand)})")
+        print(f"Your hand: {self.player_hand} (Total: {self.calculate_total(self.player_hand)})")
+
+    def check_blackjack(self):
+        if self.calculate_total(self.player_hand) == 21:
+            self.print_results()
+            print("🎉 Blackjack! You win!\n")
+            return True
+        elif self.calculate_total(self.dealer_hand) == 21:
+            self.print_results()
+            print("😞 Dealer has Blackjack. You lose.\n")
+            return True
+        return False
+
+    def evaluate_score(self):
+        self.print_results()
+        player_total = self.calculate_total(self.player_hand)
+        dealer_total = self.calculate_total(self.dealer_hand)
+
+        if player_total > 21:
+            print("😵 You busted. Dealer wins.\n")
+        elif dealer_total > 21:
+            print("🎉 Dealer busted. You win!\n")
+        elif player_total > dealer_total:
+            print("🎉 You beat the dealer!\n")
+        elif player_total < dealer_total:
+            print("😞 Dealer wins.\n")
+        else:
+            print("🤝 It's a tie!\n")
+
+    def play(self):
+        self.clear_screen()
+        print("🃏 WELCOME TO BLACKJACK 🃏\n")
+        self.dealer_hand = self.deal_hand()
+        self.player_hand = self.deal_hand()
+
+        while True:
+            print(f"Dealer shows: {self.dealer_hand[0]}")
+            print(f"Your hand: {self.player_hand} (Total: {self.calculate_total(self.player_hand)})")
+
+            if self.check_blackjack():
+                break
+
+            choice = input("Do you want to [H]it, [S]tand, or [Q]uit? ").lower()
+            if choice == "h":
+                self.player_hand.append(self.deal_card())
+                if self.calculate_total(self.player_hand) > 21:
+                    break
+            elif choice == "s":
+                while self.calculate_total(self.dealer_hand) < 17:
+                    self.dealer_hand.append(self.deal_card())
+                break
+            elif choice == "q":
+                print("👋 Thanks for playing!")
+                exit()
+
+        self.evaluate_score()
+        self.play_again()
+
+    def play_again(self):
+        again = input("Play again? (Y/N): ").lower()
+        if again == "y":
+            self.__init__()
+            self.play()
+        else:
+            print("👋 Goodbye!")
+            exit()
+
+if __name__ == "__main__":
+    game = BlackjackGame()
+    game.play()
+   # yorum 
